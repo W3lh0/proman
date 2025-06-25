8791a4b562063b88bd2d42820d9f06770bf3d842
W3lh0 <e2101506@edu.vamk.fi>
2025-06-25 10:40:58 +0300

    feat: Database connection and adding mock data to e2101506_proman database

---
+++ data/content.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects
+VALUES
+    (1, 'Learn SQL', 'School'),
+    (2, 'Learn Javascript', 'School'),
+    (3, 'Project X', 'Personal');
+
+INSERT INTO tasks
+VALUES
+    (1, 'Task 1', '2025-11-01', 25, 1),
+    (2, 'Task 2', '2025-11-15', 45, 2),
+    (3, 'Task 3', '2025-12-09', 25, 3);
