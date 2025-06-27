ac6f52ed063d50b306bd5ad3313cf42622523b6b
W3lh0 <e2101506@edu.vamk.fi>
2025-06-27 11:55:04 +0300

    feat: Implement project listing view
    This commit introduces the project_list.php view file.
    It is responsible for:
    - Displaying a list of projects, dynamically populated from data.
    - Showing a message if no projects have been added yet.
    - Providing navigation links (Go Home, Add Project).
    - Utilizing output buffering to capture content for the main layout.
    - Integrating with layout.php for consistent page structure.

---
+++ views/project_list.php
@@ -0,0 +1,31 @@
+<?php
+$title = 'Projects list';
+
+ob_start();
+?>
+
+<div class="container">
+    <p><a href="../">Go Home</a></p>
+
+    <h1><?php echo $title . " (" . $projectCount . ")" ?></h1>
+    <!-- if there's not data yet -->
+    <?php if ($projectCount == 0) { ?>
+    <div>
+        <p> You have not yet added any projects </p>
+        <p><a href='../controllers/project.php'></a></p>
+    </div>
+    <?php } ?>
+
+    <ul>
+        <?php foreach ($projects as $project) : ?>
+        <li>
+            <?php echo $project["title"] ?>
+        </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+
+<?php
+$content = ob_get_clean();
+include 'layout.php'
+?>
