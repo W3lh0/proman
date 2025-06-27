88806aa68bc571b1d2737e53fc2089fba7c53495
W3lh0 <e2101506@edu.vamk.fi>
2025-06-27 10:12:05 +0300

    feat: Add project_list controller
    This commit introduces the project_list.php controller.
    It is responsible for:
    - Including the model (model.php) to interact with data.
    - Fetching all projects and their count from the model.
    - Loading the view (project_list.php) to display the data to the user.
    
    This file serves as the main controller for the project listing page, adhering to the MVC pattern.

---
+++ controllers/project_list.php
@@ -0,0 +1,9 @@
+<?php
+// conrollers/project_list.php
+require_once "../model/model.php";
+
+$projects = get_all_projects();
+$projectCount = get_all_projects_count();
+
+require "../view/project_list.php";
+?>
