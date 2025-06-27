1798d21b007d5c7fc76e5a3189be11b6f437cf61
W3lh0 <e2101506@edu.vamk.fi>
2025-06-27 12:57:17 +0300

    feat: Added nav bar for navigation

---
+++ views/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="/~e2101506/php/proman/">theTrackerApp</a></li>
+        <li><a href="controllers/project_list.php">Project List<a></li>
+        <li><a href="controllers/task_list.php">Task list</a></li>
+    </ul>
+</nav>
