e7159d205944b495b3fcba556c7b424c4938dc1f
W3lh0 <e2101506@edu.vamk.fi>
2025-06-27 10:51:21 +0300

    feat: Add project model functionalities and exception handling
    This commit introduces the model/model.php file, which includes the following functionalities:
    - get_all_projects(): Retrieves all projects from the database, ordered by title.
    - get_all_projects_count(): Calculates and returns the total number of projects.
    - Utilizes connection.php to establish a database connection.
    - Both functions incorporate basic exception handling (try-catch) for PDO-related errors, enhancing application stability and error reporting.

---
+++ model/model.php
@@ -0,0 +1,37 @@
+<?php
+// model/model.php
+require "connection.php";
+
+$connection = db_connect();
+
+function get_all_projects()
+{
+    try {
+        global $connection;
+
+        $sql = 'SELECT * FROM projects ORDER BY title';
+        $projects = $connection->query($sql);
+
+        return $projects;
+    } catch (PDOException $err) {
+        echo $sql . "<br>" . $err->getMessage();
+        exit;
+    }
+}
+
+function get_all_projects_count()
+{
+    try {
+        global $connection;
+
+        $sql = 'SELECT COUNT(id) AS nb FROM projects';
+        $statement = $connection->query($sql)->fetch();
+
+        $projectCount = $statement['nb'];
+
+        return $projectCount;
+    } catch (PDOException $err) {
+        echo $sql . "<br>" . $err->getMessage();
+        exit;
+    }
+}
