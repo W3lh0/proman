79c4e3049db4242b5a0f8472fc1f1de1888e1c3c
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 12:04:23 +0300

    fix(layout.php): Fixed path for styles.css

--- views/layout.php
+++ views/layout.php
@@ -8,8 +8,7 @@ require_once __DIR__ . '/../utils/common.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/styles.css">
-    <link rel="stylesheet" href="../public/css/styles.css">
+    <link rel="stylesheet" href="/~e2101506/php/proman/public/css/styles.css">
     <title><?php echo escape($title) ?></title>
 </head>
 
