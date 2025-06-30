6c774095f51d37e11f4d29283bab78aea8477138
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 08:58:29 +0300

    Feat(task_list.php): Applied escape() function from utils/common.php to views/task_list.php

--- views/task_list.php
+++ views/task_list.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once '../utils/common.php';
+
 $title = 'Task list';
 
 ob_start();
@@ -8,7 +11,7 @@ require 'nav.php';
 <div class="container">
     <p><a href="../">Go Home</a></p>
 
-    <h1><?php echo $title . " (" . $taskCount . ")" ?></h1>
+    <h1><?php echo escape($title) . " (" . escape($taskCount) . ")" ?></h1>
     <!-- if there is no data yet -->
     <?php if ($taskCount == 0) { ?>
     <div>
@@ -20,7 +23,7 @@ require 'nav.php';
     <ul>
         <?php foreach ($tasks as $task) : ?>
         <li>
-            Title: <?php echo htmlspecialchars($task["TaskTitle"]); ?> (Date: <?php echo htmlspecialchars($task["data_task"]); ?>, Project: <?php echo htmlspecialchars($task["ProjectTitle"]); ?>)
+            Title: <?php echo escape($task["TaskTitle"]); ?> (Date: <?php echo escape($task["data_task"]); ?>, Project: <?php echo escape($task["ProjectTitle"]); ?>)
         </li>
         <?php endforeach; ?>
     </ul>
