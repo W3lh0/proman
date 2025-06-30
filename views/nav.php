3171fd7aa01372980dee9307977cf09b10345141
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 07:22:38 +0300

    Fix(navigation): Added title for nav bars link

--- views/nav.php
+++ views/nav.php
@@ -3,6 +3,6 @@
         <li><a href="/~e2101506/php/proman/">theTrackerApp</a></li>
         <li><a href="/~e2101506/php/proman/controllers/project_list.php">Project List<a></li>
         <li><a href="/~e2101506/php/proman/controllers/task_list.php">Task list</a></li>
-        <li><a href="/~e2101506/php/proman/controllers/project.php"></a></li>
+        <li><a href="/~e2101506/php/proman/controllers/project.php">Add project</a></li>
     </ul>
 </nav>
