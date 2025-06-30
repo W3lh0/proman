fe655a8ba7c9e74b0c9977d5bec685a8cc10074c
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 07:14:23 +0300

    Feat: Add new project creation functionality
    Implements basic project creation with title and category fields. Includes input validation and redirection to project list upon successful addition.

---
+++ controllers/project.php
@@ -0,0 +1,17 @@
+<?php
+require_once "../model/model.php";
+
+if (isset($_POST['submit'])) {
+    $title = trim($_POST['title']);
+    $category = $_POST['category'];
+
+    if (empty($title) || empty($category)) {
+        $error_message = "Title or category empty";
+    } else {
+        add_project($title, $category);
+        header('Refresh:4; url=project_list.php');
+        $confirm_message = 'Project added successfully! Moving to project list..';
+    }
+}
+
+require "../views/project.php";
