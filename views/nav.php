1518c4e95798f450b638be6ad0e4fcdc63be3b33
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 07:18:05 +0300

    Feat: Added controller/project.php to nav bar

--- views/nav.php
+++ views/nav.php
@@ -1,7 +1,8 @@
 <nav>
     <ul>
         <li><a href="/~e2101506/php/proman/">theTrackerApp</a></li>
-        <li><a href="controllers/project_list.php">Project List<a></li>
-        <li><a href="controllers/task_list.php">Task list</a></li>
+        <li><a href="/~e2101506/php/proman/controllers/project_list.php">Project List<a></li>
+        <li><a href="/~e2101506/php/proman/controllers/task_list.php">Task list</a></li>
+        <li><a href="/~e2101506/php/proman/controllers/project.php"></a></li>
     </ul>
 </nav>
