d909fb837f88d09c9290c05d5413a8757a7716f8
W3lh0 <e2101506@edu.vamk.fi>
2025-06-30 13:50:41 +0300

    Feat: Implement unique title constraints to structure.sql

--- data/structure.sql
+++ data/structure.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS tasks, projects;
 
 CREATE TABLE projects (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL UNIQUE,
     category VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE tasks (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL UNIQUE,
     data_task VARCHAR(100) NOT NULL,
     time_task INT(3) NOT NULL,
     project_id INT(11) NOT NULL,
