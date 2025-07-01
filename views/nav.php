824b046165b8b8af4846ba286e6e90c7eabb321f
W3lh0 <e2101506@edu.vamk.fi>
2025-07-01 09:54:23 +0300

    Feat(nav.php): Add link to add task to database true controller/task.php.

--- views/nav.php
+++ views/nav.php
@@ -4,5 +4,6 @@
         <li><a href="/~e2101506/php/proman/controllers/project_list.php">Project List<a></li>
         <li><a href="/~e2101506/php/proman/controllers/task_list.php">Task list</a></li>
         <li><a href="/~e2101506/php/proman/controllers/project.php">Add project</a></li>
+        <li><a href="/~e2101506/php/proman/controllers/task.php">Add task</a></li>
     </ul>
 </nav>
