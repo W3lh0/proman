f76df9fc4e4824a13f1e5aa00ae3771175bde961
W3lh0 <e2101506@edu.vamk.fi>
2025-07-02 12:36:43 +0300

    feat: Add task editing link and improve navigation links
    This commit introduces the ability to navigate to a task editing page by adding a dedicated link for each task in the task list. The link passes the task's ID as a URL parameter, preparing for the task editing functionality.
    
    Additionally, this commit includes minor improvements to existing navigation links:
    - Corrected the HTML syntax for the Go Home link.
    - Added visible text to the Add Task link, making it clickable and discoverable.

--- views/task_list.php
+++ views/task_list.php
@@ -1,32 +1,34 @@
+views/_task_list.php
 <?php
-
-require_once '../utils/common.php';
+require_once './utils/common.php';
 
 $title = 'Task list';
 
 ob_start();
 require 'nav.php';
 ?>
-
 <div class="container">
-    <p><a href="../">Go Home</a></p>
+  <p><a href="../../">Go Home</a></p>
 
-    <h1><?php echo escape($title) . " (" . escape($taskCount) . ")" ?></h1>
-    <!-- if there is no data yet -->
-    <?php if ($taskCount == 0) { ?>
-    <div>
-        <p> You have not yet added any tasks </p>
-        <p><a href='../controllers/task.php'></a></p>
-    </div>
-    <?php } ?>
+  <h1><?php echo escape($title) . " (" . escape($taskCount) . ")"; ?></h1>
+  <!-- If there is no data yet -->
+  <?php if ($taskCount == 0) { ?>
+  <div>
+    <p>You have not yet added any tasks</p>
+    <p><a href="../controllers/task.php">Add Task</a></p>
+  </div>
+  <?php } ?>
 
-    <ul>
-        <?php foreach ($tasks as $task) : ?>
-        <li>
-            Title: <?php echo escape($task["TaskTitle"]); ?> (Date: <?php echo escape($task["data_task"]); ?>, Project: <?php echo escape($task["ProjectTitle"]); ?>)
-        </li>
-        <?php endforeach; ?>
-    </ul>
+  <ul>
+  <?php foreach ($tasks as $task) : ?>
+    <li>
+        <a href="../controllers/task.php?id=<?php echo $task['id']; ?>">
+            Title: <?php echo escape($task["TaskTitle"]); ?> (Date: <?php echo escape($task["data_task"]); ?>), Project: <?php echo escape($task["ProjectTitle"]); ?>
+        </a>
+    </li>
+  <?php endforeach; ?>
+  </ul>
+
 </div>
 
 <?php
